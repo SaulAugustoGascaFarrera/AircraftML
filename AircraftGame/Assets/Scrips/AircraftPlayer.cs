f97d565b0dfef8b9d01a2dbede63c47fa1692083
Saul Augusto Gasca Farrera <sgfarreradev@gmail.com>
2023-05-04 15:28:26 -0600

    Setup the aircraft area with checkpoints prefabs and aircraft player setup finished with input bindings and heuristic function

--- AircraftGame/Assets/Scrips/AircraftPlayer.cs
+++ AircraftGame/Assets/Scrips/AircraftPlayer.cs
@@ -1,3 +1,5 @@
+using Google.Protobuf.WellKnownTypes;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -26,6 +28,34 @@ namespace Aircraft
             boostInput.Enable();
             pauseInput.Enable();
         }
+
+        /// <summary>
+        /// Reads player input and converrts it to a vector action array
+        /// </summary>
+        /// <param name="actionsOut">An array of floats for AgentAction to use</param>
+        public override void Heuristic(float[] actionsOut)
+        {
+            //Pitch: 1 == up,0 == none, -1 == down
+            float pitchValue = Mathf.Round(pitchInput.ReadValue<float>());
+
+            //Yaw 1 == turn right,0 == none,-1 == turn left
+            float yawValue = Mathf.Round(yawInput.ReadValue<float>());
+
+            //Boost 1 == bost,0 == no boost
+            float boostValue = Mathf.Round(boostInput.ReadValue<float>());
+
+            //convert -1 (down) to discrete value 2
+            if(pitchValue == -1f) pitchValue = 2f;
+
+            //convert -1 (left) to discrete value 2
+            if (yawValue == -1f) yawValue = 2f;
+
+            actionsOut[0] = pitchValue;
+            actionsOut[1] = yawValue;
+            actionsOut[2] = boostValue;
+        }
+
+
 
         /// <summary>
         /// Cleans up the inputs when destroyed
